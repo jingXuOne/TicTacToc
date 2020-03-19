59f6c888816894d3a392f676034aab859f36f233
Jing XU <jing.xu@photobox.com>
2020-03-19 12:12:46 +0100

    Only css

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Board from './Board';
+import "./styles.css";
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+    <Board />,
+  rootElement
+);
+
